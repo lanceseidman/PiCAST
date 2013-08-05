8435bf60de8f87bdd9676c2be1965e9064298261
lanceseidman <lance@compulsivetech.biz>
2013-08-04 18:51:08 -0700

    Create installer.sh
    Created Header for PiCAST Installer

---
+++ installer.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+##############################################################################
+#                   PiCAST Install & Config Helper                           #
+#                                                                            #
+# Auto-Installer for PiCAST to make PiCAST easy for everyone to use...       #
+#                                                                            #
+# NEW PI? FIRST RUN: sudo apt-get update. Run this command before Installer! #
+#                                                                            #
+# Useage:                                                                    #
+# sudo sh installer.sh                                                       #
+#                                                                            #
+##############################################################################
