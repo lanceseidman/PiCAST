db564ef0474cb6faf70b1e6c6e93aeeab91a05f0
xeaon <kemal@soyguder.de>
2013-08-05 09:57:50 +0200

    Update goCAST.php
    idea, how to realize "one task per request"

--- goCAST.php
+++ goCAST.php
@@ -12,33 +12,48 @@ if($_GET["website"])
  // IT EXISTS! LET'S STORE IT FOR OUR DATABASE QUERY
  // - WE'LL USE MIDORI AS FULLSCREEN BROWSER...
  $website = "midori -e Fullscreen -a ".$_GET["website"];
+ $type = "website";
  //Debug: echo 'Found Website, going to process it!";
 }
 
 if($_GET["youtube"]) 
 {
  $youtube = $_GET["youtube"];
+ $type = "youtube";
  //Debug: echo "Found YT Video!";
 }
 
 if($_GET["image"]) 
 {
  $image = $_GET["image"];
+ $type = "image";
  //Debug: echo "Found Image!";
 }
 
 if($_GET["map"]) 
 {
  $map = $_GET["map"];
- //Debug: echo "Found Map!";
+ $type = "map"; 
+//Debug: echo "Found Map!";
 }
 
 if($_GET["music"]) 
 {
  $music = $_GET["music"];
+ $type = "music";
  //Debug: echo "Found Music!";
 }
 
+if(is_array($_GET)) {
+    foreach($_GET as $key=>$element) {
+        if ($key != $type) {
+            $i++;
+        }
+    }
+    if ($i>0) {
+        //Error: Error - only one task allowed per request! 
+    }
+}
 
 /* MAKE SURE ONLY 1 TASK IS RECEIVED!
 if($image . $map . $music)
