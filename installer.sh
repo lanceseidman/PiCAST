83bda12ec27352ba36cdaed6ecf9497c18e670d9
lanceseidman <lance@compulsivetech.biz>
2013-08-06 20:41:45 +0000

    Update installer.sh
    Temp. Save

--- installer.sh
+++ installer.sh
@@ -11,3 +11,27 @@
 # sudo sh installer.sh                                                       #
 #                                                                            #
 ##############################################################################
+
+# STORING VARIABLES...
+# COLOR RED (Use if Root User)
+clrRED="\033[0;31m"
+# CLEAR COLOR ASSIGNMENTS
+clrCLEAN="\033[0m"
+
+# START CHECK OF ROOT LOGIN (NOT COMPLETED)
+#if [[ $users == root]]; then
+#echo $clrRED"PiCAST Error: Can't be root during setup!"$clrCLEAN
+#exit 0
+#fi
+
+# NON-ROOT LOGIN; PROCEED SETUP
+echo "Cleaning old APT Cache..."
+aptitude clean
+
+echo "Removing old Configs & Emptying Trash"
+sudo aptitude purge $OLDCONF
+#/
+#if [[ $dpkg-query -f --show <pkg> 2>/dev/null == <pkg>]]; then
+#echo "exists!"
+#fi
+
