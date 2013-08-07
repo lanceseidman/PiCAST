348fbfa1ac34fa536425e9262bf7543f23afa3cd
lanceseidman <lance@compulsivetech.biz>
2013-08-06 21:15:35 -0700

    INSTALLER FOR MIN. REQ.
    Added base of new Installer...

--- installer.sh
+++ installer.sh
@@ -1,37 +1,46 @@
-#!/bin/sh
+#!bin/sh
+# INSTALLER/SCRIPT CANNOT BE EDITED NOR DUPLICATED FOR ANY PROJECT.
 
-##############################################################################
-#                   PiCAST Install & Config Helper                           #
-#                                                                            #
-# Auto-Installer for PiCAST to make PiCAST easy for everyone to use...       #
-#                                                                            #
-# NEW PI? FIRST RUN: sudo apt-get update. Run this command before Installer! #
-#                                                                            #
-# Useage:                                                                    #
-# sudo sh installer.sh                                                       #
-#                                                                            #
-##############################################################################
-
-# STORING VARIABLES...
 # COLOR RED (Use if Root User)
 clrRED="\033[0;31m"
-# CLEAR COLOR ASSIGNMENTS
+# CLEAR COLOR
 clrCLEAN="\033[0m"
+# REQUIRED
+reqApps="apache2 apache2 apache2-utils libapache2-mod-php5 php5 php-pear php5-xcache php5-mysql mysql-server mysql-client gnash synergy"
 
-# START CHECK OF ROOT LOGIN (NOT COMPLETED)
-#if [[ $users == root]]; then
-#echo $clrRED"PiCAST Error: Can't be root during setup!"$clrCLEAN
-#exit 0
-#fi
-
+# START CHECK OF ROOT LOGIN (NO COMPLETED)
+if [ $(whoami) = "root" ]
+then
+echo $clrRED"Sorry, please run as Pi or non-root account!"$clrCLEAN
+exit 1
+else
 # NON-ROOT LOGIN; PROCEED SETUP
+clear # Let's clear the Window!
+echo "############################################"
+echo "#       WELCOME TO PICAST AUTO-SETUP!      #"
+echo "#               by Lance Seidman           #"
+echo "#------------------------------------------#"
+echo "# I'm here to perform some tasks, so try   #"
+echo "# to hold on a few moments... I'll let you #"
+echo "# know what's going on as I do them...     #"
+echo "#------------------------------------------#"
+echo "# Without further ado, let's get started!  #"
+echo "#            Hang tight... Or LOOSE!       #"
+echo "############################################"
+echo ""
+echo "          I'm going to start now...."
+echo ""
 echo "Cleaning old APT Cache..."
-aptitude clean
+sudo aptitude clean
 
-echo "Removing old Configs & Emptying Trash"
+echo "Removing old Configs..."
 sudo aptitude purge $OLDCONF
-#/
-#if [[ $dpkg-query -f --show <pkg> 2>/dev/null == <pkg>]]; then
-#echo "exists!"
-#fi
 
+# START APP INSTALL
+echo "Go Play! Go Watch TV/YouTube, just don't touch!"
+echo "PiCAST is installing everything you need..."
+sudo aptitude install -y $reqApps
+
+# COPY EVERYTHING FOR PICAST
+
+fi
