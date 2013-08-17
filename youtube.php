845ac865dfd4b4518b8d5b2c8adbfedb3a72daba
Lance <lance@compulsivetech.biz>
2013-08-17 15:30:44 -0700

    Update youtube.php
    Changed double quotes on "src". Noted by GitHub user.

--- youtube.php
+++ youtube.php
@@ -6,7 +6,7 @@ body{ margin: 0; }
 </head>
 <body>
 
-<iframe id="player" type="text/html" style='position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; z-index: 999;'  src="http://youtube.com/embed/<?php echo $_GET["youtube""]; ?>?enablejsapi=1&
+<iframe id="player" type="text/html" style='position: absolute; top: 0px; left: 0px; width: 100%; height: 100%; z-index: 999;'  src="http://youtube.com/embed/<?php echo $_GET["youtube"]; ?>?enablejsapi=1&
 autoplay=1&autohide=1&controls=0&iv_load_policy=3&modestbranding=1&theme=dark&html5=1" frameborder="0"></iframe>
 </body>
 </html>
