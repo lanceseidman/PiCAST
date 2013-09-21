231665443c1029ebd3771e2b4d77241a9e49c825
Lance <lance@compulsivetech.biz>
2013-09-20 17:05:44 -0700

    Update grabCAST.php
    Ben mentioned the fix as a " was missing: https://github.com/lanceseidman/PiCAST/commit/4d08954420fb8857aabe093cd4c4afdc92df80d6

--- grabCAST.php
+++ grabCAST.php
@@ -2,7 +2,7 @@
 // GRAB PiCAST
 
 // CONNECT TO YOUR DB
-$res = mysql_connect("localhost","picast,"piCAST1337");
+$res = mysql_connect("localhost","picast","piCAST1337");
 // CONNECT SELECT THE DB
 mysql_select_db("picaster", $res);
 
