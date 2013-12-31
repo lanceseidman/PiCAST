075a4c15e0ac152b8e269254347a06e6e89b1111
lanceseidman <lance@compulsivetech.biz>
2013-12-31 14:38:37 -0800

    Adding New Files

---
+++ youtube/yt-midori.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Going to Stream YouTube via Midori"
+sleep 2
+for URL in "$@"
+do
+midori -e Fullscreen -a http://localhost/youtube.php?youtube= $URL
+echo "Should be opened, bye!"
+done
