aedeb38d0d1586b0f798b092984e25ce0e38cf7f
lanceseidman <lance@compulsivetech.biz>
2013-12-31 14:50:28 -0800

    Fixed YT Audio Download & Launching

--- youtube/yt-musicgrab.sh
+++ youtube/yt-musicgrab.sh
@@ -3,6 +3,7 @@ echo "Going to grab YouTube Audio..."
 sleep 2
 for URL in "$@"
 do
-youtube-dl -t --extract-audio --audio-format mp3 $URL
+youtube-dl -t --extract-audio --audio-format mp3 $URL ytaudio.mp3
+mplayer -fs ytaudio.mp3
 echo "Have a nice day!"
 done
