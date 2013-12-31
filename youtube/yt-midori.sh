ab94715fe409b5ba3f128e28558ffd9c117baaf7
lanceseidman <lance@compulsivetech.biz>
2013-12-31 14:41:17 -0800

    Modified Feature to be Disabled

--- youtube/yt-midori.sh
+++ youtube/yt-midori.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-echo "Going to Stream YouTube via Midori"
+echo "Feature not Available Yet..."
+#echo "Going to Stream YouTube via Midori"
 sleep 2
-for URL in "$@"
-do
-midori -e Fullscreen -a http://localhost/youtube.php?youtube= $URL
-echo "Should be opened, bye!"
-done
+#for URL in "$@"
+#do
+#midori -e Fullscreen -a http://localhost/youtube.php?youtube= $URL
+#echo "Should be opened, bye!"
+#done
