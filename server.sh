f1bd0147f2df425d6a5cccf536fe348089eb46b4
lanceseidman <lance@compulsivetech.biz>
2014-01-15 22:14:57 -0800

    Update from Hospital via Home via SSH. I'll fix it later!

---
+++ server.sh
@@ -0,0 +1,23 @@
+############################################
+# PiCAST Server/Listener
+# ------------------------------------------
+# Setup: chmod +x server.sh
+# Start Command: sh /server.sh
+############################################
+
+# Port is 2555; Modify for Firewall/Pref.
+
+echo "Welcome to PiCAST!"
+echo "------------------"
+theIP=`/sbin/ifconfig wlan0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+
+if [ $theIP != null ]; then
+echo "To run, please send requests to IP: "$theIP
+echo "+===================================================+"
+echo "Example: pic_ytvideo <URL/VIDEO> |"
+else
+theIP=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+echo "To run, please send requests to IP: "$theIP
+echo "+===================================================+"
+echo "Example: pic_ytvideo <URL/VIDEO> | "
+fi
--- youtube/yt-musicgrab.sh
+++ youtube/yt-musicgrab.sh
@@ -3,7 +3,10 @@ echo "Going to grab YouTube Audio..."
 sleep 2
 for URL in "$@"
 do
-youtube-dl -t --extract-audio --audio-format mp3 $URL ytaudio.mp3
-mplayer -fs ytaudio.mp3
+youtube-dl -t --extract-audio --audio-format mp3 $URL
+for f in $( find . -name "mp4" ); do
+echo $f
+mplayer -fs $f
+done
 echo "Have a nice day!"
 done
