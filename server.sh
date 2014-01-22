79c8d85669e46f6384294c267b7192532858e723
lanceseidman <lance@compulsivetech.biz>
2014-01-22 12:55:21 -0800

    PiCAST Server

--- server.sh
+++ server.sh
@@ -15,10 +15,6 @@ set port "8882" # soon...
 #echo "Reading Config File for Settings..."
 pass=$(awk "NR==1 {print;exit}" ./con.cfg)
 
-# Let's start the process of keeping me ALIVE!
-while true
-do
-
 echo "Welcome to PiCAST!"
 echo "------------------"
 theIP=`/sbin/ifconfig wlan0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
@@ -26,14 +22,17 @@ theIP=`/sbin/ifconfig wlan0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}
 if [ $theIP != null ]; then
 echo "To CAST, please send requests to IP: " $theIP
 echo "+===================================================+"
-echo "Example: pic_ytvideo <URL/VIDEO> | cryptcat $theIP -k picast"
+echo "Example: pic_ytvideo <URL/VIDEO> | cryptcat $theIP $port -k picast <pass>"
 else
 theIP=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
 echo "To CAST, please send requests to IP: " $theIP
 echo "+===================================================+"
-echo "Example: pic_ytvideo <URL/VIDEO> | cryptcat $theIP -k picast"
+echo "Example: pic_ytvideo <URL/VIDEO> | cryptcat $theIP $port -k picast <pass>"
 fi
 
+# Let's start the process of keeping me ALIVE!
+while true
+do
 # We're going to use the pre-defined settings above, change as needed.
 cryptcat -l -p $port -k $pass
 
