5cfb0ffdfc4e16763278f8cbbfbcbaa1d494db4a
hergin <hergin@crimson.ua.edu>
2016-02-27 12:07:05 -0600

    home directory
    PiCAST scripts should reside under home directory rather than under
    ffmpeg

--- setup.sh
+++ setup.sh
@@ -45,6 +45,7 @@ sudo npm install forever -g
 sudo npm install forever-monitor -g
 
 # GET PICAST NEEDED FILES...
+cd ~
 echo "Making PiCAST Folder..."
 mkdir PiCAST
 echo "Entering PiCAST Folder..."
